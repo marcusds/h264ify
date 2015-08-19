3784fccd9edb7cda9f0c8a43f8a5791a6e0f2de1
Marcus Schwab <marcus@14oranges.com>
2015-08-19 12:54:06 -0700

    Go a step further and force any HTML5 video to play MP4 if it exists.

--- src/inject/content_script.js
+++ src/inject/content_script.js
@@ -21,11 +21,29 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
+ 
+document.addEventListener('DOMContentLoaded', function() {
+	var videos = document.getElementsByTagName('video');
+	if(videos.length > 0) {
+		var videoLength = videos.length, i = 0;
+		for(i = 0; i < videoLength; i++) {
+			var sources = videos[i].getElementsByTagName('source');
+			var sourcesLength = sources.length, i2 = 0;
+			for(i2 = 0; i2 < sourcesLength; i2++) {
+				if(typeof sources[i2].type !== 'string') {
+					continue;
+				}
+				if(sources[i2].type === 'video/mp4') {
+					videos[i].src = sources[i2].src;
+				}
+			}
+		}
+	}
+}, false);
+ 
 var injectScript = document.createElement('script');
 injectScript.src = chrome.extension.getURL('src/inject/inject.js');
 injectScript.onload = function() {
   this.parentNode.removeChild(this);
 };
 (document.head || document.documentElement).appendChild(injectScript);
-
